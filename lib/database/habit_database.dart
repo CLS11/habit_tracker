7dc5bf59f4268f611806608cc489765fbdc1a459
CLS11 <crystall.101600@gmail.com>
2024-12-26 22:18:13 +0530

    Saving the habit database offline.

---
+++ lib/database/habit_database.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker/models/app_settings.dart';
+import 'package:habit_tracker/models/habit.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+
+class HabitDatabase extends ChangeNotifier {
+  static late Isar isar;
+
+  //Initialising the database
+  static Future<void> initialise() async {
+    final dir = await getApplicationDocumentsDirectory();
+    isar = await Isar.open(
+      [HabitSchema, AppSettingsSchema],
+      directory: dir.path,
+    );
+  }
+
+  //Saving the initial date of the app
+  Future<void> saveFirstLaunchDate() async {
+    final existingSettings = await isar.appSettings.where().findFirst();
+    if (existingSettings == null) {
+      final settings = AppSettings()..firstLaunchDate = DateTime.now();
+      await isar.writeTxn(() => isar.appSettings.put(settings));
+    }
+  }
+
+  //Fetch the initial date
+  Future<DateTime?> getFirstLaunchDate() async {
+    final settings = await isar.appSettings.where().findFirst();
+    return settings?.firstLaunchDate;
+  }
+}
