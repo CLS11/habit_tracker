ee4731d816d4f59b3d346e59c4b1b5303cd50b83
CLS11 <crystall.101600@gmail.com>
2024-12-28 22:46:24 +0530

    Added the heatmap dataset.

--- lib/utilities/habit_utilities.dart
+++ lib/utilities/habit_utilities.dart
@@ -1,3 +1,5 @@
+import 'package:habit_tracker/models/habit.dart';
+
 bool isHabitCompletedToday(List<DateTime> completedDays) {
   final today = DateTime.now();
   return completedDays.any(
@@ -6,4 +8,21 @@ bool isHabitCompletedToday(List<DateTime> completedDays) {
         date.month == DateTime.now().month &&
         date.day == DateTime.now().day,
   );
+}
+
+//Heatmap dataset
+Map<DateTime, int> prepHeatMapDatabase(List<Habit> habits) {
+  Map<DateTime, int> dataset = {};
+
+  for (var habit in habits) {
+    for (var date in habit.completedDays) {
+      final normalisedDate = DateTime(date.year, date.month, date.day);
+      if (dataset.containsKey(normalisedDate)) {
+        dataset[normalisedDate] = dataset[normalisedDate]! + 1;
+      } else {
+        dataset[normalisedDate] = 1;
+      }
+    }
+  }
+  return dataset;
 }
