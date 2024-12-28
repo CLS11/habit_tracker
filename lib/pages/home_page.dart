5a6622485443f491c42473d921726b128cb55c56
CLS11 <crystall.101600@gmail.com>
2024-12-28 22:45:35 +0530

    Added the build method for heatmap calendar to be displayed on the home screen.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/components/drawer.dart';
+import 'package:habit_tracker/components/habit_tile.dart';
+import 'package:habit_tracker/components/heat_map.dart';
 import 'package:habit_tracker/database/habit_database.dart';
 import 'package:habit_tracker/models/habit.dart';
 import 'package:habit_tracker/utilities/habit_utilities.dart';
@@ -55,11 +57,89 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  //Checking the habit
+  void checkHabitOnOff(bool value, Habit habit) {
+    //Updating the status of habit completion
+    if (value != null) {
+      context.read<HabitDatabase>().updateHabitCompletion(habit.id, value);
+    }
+  }
+
+  //Edit the habit
+  void editHabitBox(Habit habit) {
+    textController.text = habit.name;
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: textController,
+        ),
+        actions: [
+          //Save button
+          MaterialButton(
+            onPressed: () {
+              String newHabitName = textController.text;
+              context.read<HabitDatabase>().updateHabitName(
+                    habit.id,
+                    newHabitName,
+                  );
+              Navigator.pop(context);
+              textController.clear();
+            },
+            child: const Text('Edit'),
+          ),
+          //Cancel button
+          MaterialButton(
+            onPressed: () {
+              Navigator.pop(context);
+              textController.clear();
+            },
+            child: const Text('Cancel'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  //Delete the habit
+  void deleteHabitBox(Habit habit) {
+    textController.text = habit.name;
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Are you sure you want to delete?'),
+        actions: [
+          //Delete button
+          MaterialButton(
+            onPressed: () {
+              context.read<HabitDatabase>().deleteHabit(
+                    habit.id,
+                  );
+              Navigator.pop(context);
+            },
+            child: const Text('Delete'),
+          ),
+          //Cancel button
+          MaterialButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Cancel'),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      appBar: AppBar(),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
       drawer: const MyDrawer(),
       floatingActionButton: FloatingActionButton(
         onPressed: createNewHabit,
@@ -67,7 +147,33 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Theme.of(context).colorScheme.tertiary,
         child: const Icon(Icons.add),
       ),
-      body: _buildHabitList(),
+      body: ListView(
+        children: [
+          //Heatmap calendar
+          _buildHeatMap(),
+
+          //Habit list
+          _buildHabitList(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHeatMap() {
+    final habitDatabase = context.watch<HabitDatabase>();
+    List<Habit> currentHabits = habitDatabase.currentHabits;
+    return FutureBuilder<DateTime?>(
+      future: habitDatabase.getFirstLaunchDate(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return HeatMap(
+            initDate: snapshot.data!,
+            datasets: prepHeatMapDatabase(currentHabits),
+          );
+        } else {
+          return Container();
+        }
+      },
     );
   }
 
@@ -76,11 +182,17 @@ class _HomePageState extends State<HomePage> {
     List<Habit> currentHabits = habitDatabase.currentHabits;
     return ListView.builder(
       itemCount: currentHabits.length,
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
       itemBuilder: (context, index) {
         final habit = currentHabits[index];
         bool isCompletedToday = isHabitCompletedToday(habit.completedDays);
-        return ListTile(
-          title: Text(habit.name),
+        return HabitTile(
+          isCompleted: isCompletedToday,
+          text: habit.name,
+          onChanged: (value) => checkHabitOnOff(value!, habit),
+          editHabit: (context) => editHabitBox(habit),
+          deleteHabit: (context) => deleteHabitBox(habit),
         );
       },
     );
