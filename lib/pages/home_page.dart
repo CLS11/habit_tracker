8e448d8f07ec215a160d3a345e639a6503a3eb53
CLS11 <crystall.101600@gmail.com>
2024-12-28 14:25:28 +0530

    Text controller to show a dialog box in order to add the habits with a save and a cancel button.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:habit_tracker/theme/theme_provider.dart';
+import 'package:habit_tracker/components/drawer.dart';
+import 'package:habit_tracker/database/habit_database.dart';
+import 'package:habit_tracker/models/habit.dart';
+import 'package:habit_tracker/utilities/habit_utilities.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,20 +14,75 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      drawer: Drawer(
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        child: Center(
-          child: CupertinoSwitch(
-            value: Provider.of<ThemeProvider>(context).isDarkMode,
-            onChanged: (value) =>
-                Provider.of<ThemeProvider>(context, listen: false)
-                    .toggleTheme(),
+  void initState() {
+    Provider.of<HabitDatabase>(context, listen: false).readHabits();
+    super.initState();
+  }
+
+  //Text controller
+  final TextEditingController textController = TextEditingController();
+
+  //Creating the new habit
+  void createNewHabit() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: textController,
+          decoration: const InputDecoration(
+            hintText: 'Create a new habit...',
           ),
         ),
+        actions: [
+          MaterialButton(
+            onPressed: () {
+              String newHabitName = textController.text;
+              context.read<HabitDatabase>().addHabit(newHabitName);
+              Navigator.pop(context);
+              textController.clear();
+            },
+            child: const Text('Save'),
+          ),
+          MaterialButton(
+            onPressed: () {
+              Navigator.pop(context);
+              textController.clear();
+            },
+            child: const Text('Cancel'),
+          ),
+        ],
       ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: AppBar(),
+      drawer: const MyDrawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewHabit,
+        elevation: 0,
+        backgroundColor: Theme.of(context).colorScheme.tertiary,
+        child: const Icon(Icons.add),
+      ),
+      body: _buildHabitList(),
+    );
+  }
+
+  Widget _buildHabitList() {
+    final habitDatabase = context.watch<HabitDatabase>();
+    List<Habit> currentHabits = habitDatabase.currentHabits;
+    return ListView.builder(
+      itemCount: currentHabits.length,
+      itemBuilder: (context, index) {
+        final habit = currentHabits[index];
+        bool isCompletedToday = isHabitCompletedToday(habit.completedDays);
+        return ListTile(
+          title: Text(habit.name),
+        );
+      },
     );
   }
 }
