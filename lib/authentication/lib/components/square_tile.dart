e14e20a3ce5aa40110f803d1c0eee9b4f7cd5cce
CLS11 <crystall.101600@gmail.com>
2025-01-03 18:54:46 +0530

    Completed the UI by decorating the square tile by Box decoration.

--- lib/authentication/lib/components/square_tile.dart
+++ lib/authentication/lib/components/square_tile.dart
@@ -11,6 +11,13 @@ class SquareTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.white,
+        ),
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.grey[200],
+      ),
       child: Image.asset(
         imagePath,
         height: 50,
